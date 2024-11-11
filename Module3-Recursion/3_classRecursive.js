52842abd3e34b40d890287bc8ca0e8deab670c34
farnaz <farnaz.towhidi@gmail.com>
2024-11-11 08:44:41 -0500

    update recursive

--- Module3-Recursion/3_classRecursive.js
+++ Module3-Recursion/3_classRecursive.js
@@ -1,38 +1,10 @@
 
 const classList = ["Victor", "Thomas", "Mytisha", "kyle", "Matthew"];
 
-// // Recursive function to pass the message
-// function passMessage(index) {
-//   // Base Case: If we've reached the last person, stop.
-//   if (index === people.length - 1) {
-//     console.log(`${people[index]} received the message and stopped!`);
-//   } else {
-//     // Recursive Step: Pass the message to the next person
-//     console.log(`${people[index]} passed the message to ${people[index + 1]}`);
-//     passMessage(index + 1); // Recursive call with the next person in line
-//   }
-// }
-
-// Start passing the message from the first person
-// passMessage(0);
-
 function passMessage(index) {
-  console.log (`${classList[index]} to ${classList[index]} `)
+  if (index==classList.length-1) return
+  console.log (`${classList[index]} pass message to ${classList[index+1]} `)
   passMessage(index+1)
 }
 
-
-
-
-
-function passMessageRecursive (index) {
-  if (index===4) 
-      console.log ("End")
-  else {
-    debugger;
-    console.log (`person ${people[index]} passed message to ${people[index+1]}`)
-    passMessageRecursive (index+1)
-  }
-}
-
-passMessageRecursive(0);
+passMessage(0);
